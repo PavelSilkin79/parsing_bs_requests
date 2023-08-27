a57b9f620ae887713d6aa0dfbd7b707ce5984615
Pavel Silkin <pashasil-5@mail.ru>
2023-08-27 22:27:14 +0300

    add parsing page

--- pars/parsing.py
+++ pars/parsing.py
@@ -28,4 +28,59 @@ for book in books:
     books_data.append(book_dict)
     
 print(books_data[10])
+
+get_html = requests.get('https://books.toscrape.com/catalogue/page-1.html')
+if get_html.status_code == 200:
+    soup = bs(get_html.content, 'html.parser')
+    next_page = 'https://books.toscrape.com/catalogue/' + soup.find('li', attrs={'class': 'next'}).find('a')['href']
+    
+    get_next_html = requests.get(next_page)
+    if get_next_html.status_code == 200:
+        print(get_next_html.content)
+        
+def get_books(content):
+    soup = bs(content, 'html.parser')
+    ol = soup.find('ol', attrs={'class': 'row'})
+    books = ol.select('li')
+    
+    books_data = []
+    for book in books:
+        image = book.find('div', attrs={'class': 'image_container'}).find('img')['src']
+        title =book.find('h3').find('a')['title']
+        price = book.find('p', attrs={'class': 'price_color'}).text 
+        
+        book_dict = {
+			'image': image,
+        	'title': title,
+        	'price': price
+		}
+        books_data.append(book_dict)
+    return books_data
    
+def get_next_page(content):
+    soup = bs(content, 'html.parser')
+    try:
+        next_page = 'https://books.toscrape.com/catalogue/' + soup.find('li', attrs={'class': 'next'}).find('a')['href']
+        return next_page
+    except:
+        pass
+    
+final_data = []
+page_number = 1
+url = 'https://books.toscrape.com/catalogue/page-1.html'
+get_html = requests.get(url)
+if get_html.status_code == 200:
+    while True:
+        books = get_books(get_html.content)
+        print(f'получено {len(books)} с {page_number} страницы')
+        final_data += books
+        
+        next_page = get_next_page(get_html.content)
+        if next_page:
+            page_number += 1
+            get_html = requests.get(next_page)
+            if get_html.status_code == 200:
+                print(f'начинаем парсить {page_number}')
+        else:
+            break
+print(f'Данные спарсились: {page_number} страниц, {len(final_data)} книг')        
